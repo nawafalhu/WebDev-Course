f286ad5064a08da91962c2d934dd56488ff3cac9
nawaf <n.alhu10@gmail.com>
2024-02-14 18:27:18 +0300

    lab3 part3

---
+++ Lab/Lab3/Lab3-Part3.py
@@ -0,0 +1,10 @@
+"""
+Write a program that prompts the user to input a Celsius temperature and 
+outputs the equivalent temperature in Fahrenheit
+"""
+
+Celsius = int(input("Enter a Celsius temperature\n:>"))
+
+fah =  (Celsius * 9/5) + 32
+
+print(f"the temperature in Fahrenheit is {fah}")
