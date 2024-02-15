28ca77bbc121bdb1d0c53a6d551b333d5a33168b
nawaf <n.alhu10@gmail.com>
2024-02-15 18:43:48 +0300

    lab3 part5

--- Lab/Lab3/Lab3-Part5.py
+++ Lab/Lab3/Lab3-Part5.py
@@ -1,9 +1,15 @@
-x = int(input("Enter a number of repetitions"))
+x = int(input("Enter a number of repetitions: "))
 
+def repeat_hello(x):
+    def decorator(func):
+        def wrapper():
+            for _ in range(x):
+                func()
+        return wrapper
+    return decorator
+
+@repeat_hello(x)
 def hello():
-       print ('Hello')
+    print('Hello')
 
-
-# Write your decorator here
-for i in range(1, x + 1):
-       hello()
+hello()
